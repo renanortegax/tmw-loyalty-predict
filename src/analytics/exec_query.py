43ffdcc2ab67038f858a3d661cb5ce6b1616aa0a
renanortegax <renan.xavier44@gmail.com>
2026-01-11 20:22:33 -0300

    Add: transformando o exec_query para executar dinamico via terminal

--- src/analytics/exec_query.py
+++ src/analytics/exec_query.py
@@ -4,6 +4,8 @@ import sqlalchemy
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from tqdm import tqdm
+import argparse
+import os
 
 #%%
 def import_query(path):
@@ -26,7 +28,7 @@ def generate_dates_day_one(date_start, date_end):
         
     return dates, [d.strftime("%Y-%m-%d") for d in dates]
 
-def generate_dates(date_start, date_end):
+def generate_dates(date_start, date_end, monthly=False):
     if isinstance(date_start, str):
         date_start = datetime.strptime(date_start, "%Y-%m-%d").date()
     if isinstance(date_end, str):
@@ -37,44 +39,62 @@ def generate_dates(date_start, date_end):
     while date_start <= date_end:
         dates.append(date_start)
         date_start += relativedelta(days=1)
-        
-    return dates, [d.strftime("%Y-%m-%d") for d in dates]
 
+    if monthly:
+        dates = [d for d in dates if d.day == 1]
+        return dates, [d.strftime("%Y-%m-%d") for d in dates]
+        
+
+    return dates, [d.strftime("%Y-%m-%d") for d in dates]
+#%%
+dicts_files = {f.split(".")[-2] : ".".join(f.split(".")[:-2]) for f in os.listdir("./") if f.endswith(".sql")}
+
+#%%
+def exec_query(table, db_origin, db_target, dt_start, dt_stop, monthly):
+    engine_app = sqlalchemy.create_engine(f"sqlite:///../../data/{db_origin}/database.db")
+    engine_analytical = sqlalchemy.create_engine(f"sqlite:///../../data/{db_target}/database.db")
+
+    dates = generate_dates(dt_start, dt_stop, monthly)[-1]
+    query = import_query(f'./{dicts_files.get(table)}.{table}.sql')
+    df = pd.read_sql(query.format(date='2025-11-09'), engine_app)
+    df.head()
+
+    # Foto da base todo final de mês
+    for d in tqdm(dates):
+        query_format = query.format(date=d)
+        
+        with engine_analytical.connect() as con:
+            try:
+                query_delete = f"delete from {table} where data_ref = date('{d}', '-1 day')"
+                con.execute(sqlalchemy.text(query_delete))
+                con.commit()
+            except Exception as e:
+                print(e)
+        
+        # print(d)
+        df = pd.read_sql(query_format, engine_app)
+        df.to_sql(table, engine_analytical, index=False, if_exists='append')
 
 # dates = generate_dates('2024-03-01', '2025-09-01')[-1]
-dates = generate_dates('2025-09-01', '2025-10-01')[-1]
 
-#%%
-query = import_query('./03.life_cycle.sql')
-# print(query.format(date='2025-11-09'))
-
-#%%
-engine_app = sqlalchemy.create_engine("sqlite:///../../data/loyalty-system/database.db")
-engine_analytical = sqlalchemy.create_engine("sqlite:///../../data/analytics/database.db")
-#%%
-df = pd.read_sql(query.format(date='2025-11-09'), engine_app)
-df.head()
-
-# Foto da base todo final de mês
-for d in tqdm(dates):
-    query_format = query.format(date=d)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db_origin', choices=['loyalty-system', 'education-plataform','analytics'], default='loyalty-system')
+    parser.add_argument('--db_target', choices=['analytics'], default='analytics')
+    parser.add_argument('--table', type=str, help='Tabela que vai processar com o mesmo nome do arquivo')
+    parser.add_argument('--start', type=str, default='2024-03-01')
+    parser.add_argument('--monthly', action='store_true')
     
-    with engine_analytical.connect() as con:
-        try:
-            query_delete = f"delete from life_cycle where data_ref = date('{d}', '-1 day')"
-            con.execute(sqlalchemy.text(query_delete))
-            con.commit()
-        except Exception as e:
-            print(e)
+    stop = datetime.now().strftime('%Y-%m-%d')
+    parser.add_argument('--stop', type=str, default=stop)
     
-    # print(d)
-    df = pd.read_sql(query_format, engine_app)
-    df.to_sql("life_cycle", engine_analytical, index=False, if_exists='append')
+    
+    args = parser.parse_args()
+    
+    print(f"Iniciando para os seguintes argumentos:\n{args}")
+    
+    exec_query(args.table,args.db_origin,args.db_target,args.start,args.stop,args.monthly)
+    
+if __name__ == "__main__":
+    main()
 
-#%%
-# ------------- DROPANDO A TABELA -------------
-# metadata = sqlalchemy.MetaData()
-# your_table = sqlalchemy.Table('life_cycle', metadata, autoload_with=engine_analytical)
-# your_table.drop(engine_analytical)
-# print("Table 'life_cycle' dropped successfully!")
-###############################################
