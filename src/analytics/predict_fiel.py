55ee9495bf8d31395abc50aa2efcb4cfad5a0a6a
renanortegax <renan.xavier44@gmail.com>
2026-01-20 22:43:06 -0300

    Add: altera modo para append da tabela de predicts para loggar historico

--- src/analytics/predict_fiel.py
+++ src/analytics/predict_fiel.py
@@ -21,6 +21,18 @@ data['predict_fiel'] = predict #probabilidade de ser fiel
 data = data[['data_ref','idcliente','predict_fiel']]
 
 #%%
-data.to_sql("predict_score_fiel", con, index=False, if_exists='replace')
+data_ref_processed = data['data_ref'].max()
+
+#%%
+with con.connect() as conn:
+    try:
+        query_delete = f"delete from predict_score_fiel where data_ref = '{data_ref_processed}'"
+        print(query_delete)
+        conn.execute(sqlalchemy.text(query_delete))
+        conn.commit()
+    except Exception as e:
+        print(e)
+
+data.to_sql("predict_score_fiel", con, index=False, if_exists='append')
 
 #%%
